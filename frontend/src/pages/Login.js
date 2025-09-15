cd41185f783144a690a14ddaf2e4f329bb94c60e
harSh745-hi <harshpandharpatte@gmail.com>
2025-09-15 10:50:56 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Login({ onLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+     
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <form 
