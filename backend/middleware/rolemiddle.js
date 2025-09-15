e22bfd4f03106fb4de34b28f2cb86f952b5c3d5d
harSh745-hi <harshpandharpatte@gmail.com>
2025-09-15 10:43:57 +0530

    Update rolemiddle.js

--- backend/middleware/rolemiddle.js
+++ backend/middleware/rolemiddle.js
@@ -2,7 +2,7 @@ module.exports = (roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({ message: "Forbidden" });
-    }
+    } 
     next();
   };
 };
